c89f46862942c96c8543b6840bc72704b706a19a
nastiachmel <nastiachmel@gmail.com>
2019-11-18 18:55:24 +0200

    add menu,js and variables.scss

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+     
+    });
+      });
+})(jQuery);
+
+
+function acceptCookie(){
+  document.getElementsByClassName("cookie")[0].style.display="none";
+
+}
