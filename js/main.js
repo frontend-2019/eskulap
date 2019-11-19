ffa82434c8e5dd282a9bda5b15082c5022d57ab9
nastiachmel <nastiachmel@gmail.com>
2019-11-19 10:31:36 +0200

    add link, fix footer bg,fix js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 (function($){
   $(function() {
     $('.menu__icon').on('click', function() {
@@ -13,5 +11,14 @@
 
 function acceptCookie(){
   document.getElementsByClassName("cookie")[0].style.display="none";
-
 }
+
+(function($){
+  $(function() {
+    $('.nav__item').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+     
+    });
+      });
+})(jQuery);
