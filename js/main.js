ccdc92aef4092510c1dbdd62d4655619a98d7f3c
Skvibs <unknown_44@mail.ru>
2019-11-19 15:00:09 +0200

    Edit pop-up button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(function () {
-    $('.section-close').on('click', function () {
-        $('.section-pop-up').toggleClass('section__disabled');
-    });
-});
+// $(function () {
+//     $('.section-close').on('click', function () {
+//         $('.section-pop-up').toggleClass('section__disabled');
+//     });
+// });
 
 
 $(function () {
-    $('.block__toggler').on('click', function () {
+    $('.block__toggler, .section-close').on('click', function () {
         $('.section-pop-up').toggleClass('section__enable');
     });
 });
