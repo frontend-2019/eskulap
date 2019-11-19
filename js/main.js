d1d2e77373b73c9c025ba74c40653eb7bb2ecc43
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-11-19 20:40:03 +0200

    add new files header

--- js/main.js
+++ js/main.js
@@ -10,3 +10,10 @@ $(function () {
         $('.section-pop-up').toggleClass('section__enable');
     });
 });
+
+
+$(function () {
+    $('.section-block__submit, .hire-me-close').on('click', function () {
+        $('.section-hire-me').toggleClass('section__enable');
+    });
+});
